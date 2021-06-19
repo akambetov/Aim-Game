6226d9cdc2711fe89347c93e1a4083f7d11a879d
akambetov <almat_kambetov@mail.ru>
2021-06-19 18:34:54 +0600

    finish

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const screens = document.querySelectorAll('.screen')
 const timeList= document.querySelector('#time-list')
 const timeEl = document.querySelector('#time')
 const board = document.querySelector('#board')
+
+const colors = ['#FFFF33', '#FF3300', '#9900FF', '#663399', '#00FFFF', '#33FF00', '#CC6600', '#FF6600']
 let time = 0
 let score = 0
 let interval = null
@@ -43,6 +45,10 @@ function decreaseTime () {
     if (current < 10) {
       current = `0${current}`
     }
+    if (current < 4) {
+      timeEl.classList.remove('primary')
+      timeEl.classList.add('less-time')
+    }
     setTime(current)
   }
 }
@@ -54,9 +60,7 @@ function setTime (value) {
 function finishGame () {
   clearInterval(interval)
   board.innerHTML = `<h1>Счет: <span class="primary">${score}</span></h1>`
-  // timeEl.classList.add
   timeEl.parentElement.classList.add('hide')
-  console.dir(timeEl)
 }
 
 function createRandomCircle () {
@@ -65,16 +69,25 @@ function createRandomCircle () {
   const size = getRandomNumber(10, 60)
   const x = getRandomNumber(0, width - size)
   const y = getRandomNumber(0, height - size)
+  const colorGradient_1 = getRandomColor() 
+  const colorGradient_2 = getRandomColor() 
 
   circle.classList.add('circle')
   circle.style.width = `${size}px`
   circle.style.height = `${size}px`
   circle.style.top = `${y}px`
   circle.style.left = `${x}px`
+  circle.style.boxShadow = `0 0 5px ${colorGradient_1}, 0 0 10px ${colorGradient_2}`
+  circle.style.background = `linear-gradient(90deg, ${colorGradient_1}, ${colorGradient_2}`
 
   board.append(circle)
 }
 
 function getRandomNumber (min, max) {
   return Math.round(Math.random() * (max - min) + min)
+}
+
+function getRandomColor () {
+  // console.log(Math.floor(Math.random() * colors.length))
+  return colors[Math.floor(Math.random() * colors.length)]
 }
